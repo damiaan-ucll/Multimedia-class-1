0fd44d9f20aa91a65213afe69da7943f9018e22e
Devian <info@devian.tk>
2015-02-10 11:07:39 +0100

    Initial Commit

---
+++ Class 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Class 1
+//
+//  Created by Damiaan Dufaux on 10/02/15.
+//  Copyright (c) 2015 Damiaan Dufaux. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
